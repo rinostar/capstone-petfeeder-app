e0d8d5f975450648d90f9881350af5ee51ff11ee
Rinostar <codingrinostar@gmail.com>
2020-01-10 23:41:09 -0800

    npm build react-frontend

---
+++ react-frontend/build/precache-manifest.46f8b972699ff5b1a6b4e0b8deceb90f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0d861064eb038a1037ee93bbdb95731",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccbfcb6785d764ad5e1d",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "4bafdd4c5af7a40724f3",
+    "url": "/static/css/main.e61e7459.chunk.css"
+  },
+  {
+    "revision": "ccbfcb6785d764ad5e1d",
+    "url": "/static/js/2.2a98fe28.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.2a98fe28.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4bafdd4c5af7a40724f3",
+    "url": "/static/js/main.dcb3336e.chunk.js"
+  },
+  {
+    "revision": "b22a65bb62479689904e",
+    "url": "/static/js/runtime-main.9d8dd562.js"
+  }
+]);
