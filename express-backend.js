fa728a025cf855ad16e1bb5508402e00fee3f9a2
Rinostar <codingrinostar@gmail.com>
2020-01-20 18:30:18 -0800

    fixed scheudle bugs

--- express-backend.js
+++ express-backend.js
@@ -40,19 +40,14 @@ app.post('/api/appointments/add', (req, res, next) => {
   tA[1] = tA[1] - 1;
   tA.push(0);
   let timeData = new Date(tA[0], tA[1], tA[2], tA[3], tA[4], tA[5]);
-  // schedule.scheduleJob(timeData, feedN);
-  schedule.scheduleJob(timeData, () => {
+  schedule.scheduleJob(timeData, function() {
     feedN((response) => {
     let s = JSON.stringify(response.result.payload.data)
-
     const newLog = new Log({
       device: "PyPi",
       fedTime: s,
     });
-    newLog.save(err => {
-      if (err) return err;
-      else return 'Log successfully saved!';
-    });
+    newLog.save;
     })
   });
 
